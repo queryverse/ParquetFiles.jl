0c3a7f1b95c70f78f650430f1182bde23e88bed9
David Anthoff <anthoff@berkeley.edu>
2017-12-17 17:31:05 -0800

    Add initial version

--- src/ParquetFiles.jl
+++ src/ParquetFiles.jl
@@ -1,5 +1,69 @@
 module ParquetFiles
 
-# package code goes here
+using Parquet, IteratorInterfaceExtensions, TableTraits, NamedTuples
+import FileIO
+import IterableTables
+
+struct ParquetFile
+    filename::String
+end
+
+struct ParquetNamedTupleIterator{T,T_row}
+    rc::RecCursor
+    nrows::Int
+end
+
+function Base.eltype(itr::ParquetNamedTupleIterator{T,T_row}) where {T,T_row}
+    return T        
+end
+
+function Base.length(itr::ParquetNamedTupleIterator)
+    return itr.nrows
+end
+
+function Base.start(itr::ParquetNamedTupleIterator)
+    return start(itr.rc)
+end
+
+@generated function Base.next(itr::ParquetNamedTupleIterator{T,T_row}, state) where {T,T_row}
+    names = fieldnames(T)
+    x = quote
+        v, next_state = next(itr.rc, state)
+        return T($([T.types[i]<:String ? :(String(copy(v.$(names[i])))) : :(v.$(names[i])) for i=1:length(T.types)]...)), next_state
+    end
+    return x
+end
+
+function Base.done(itr::ParquetNamedTupleIterator, state)
+    return done(itr.rc, state)
+end
+
+function load(f::FileIO.File{FileIO.format"Parquet"})
+    return ParquetFile(f.filename)
+end
+
+IteratorInterfaceExtensions.isiterable(x::ParquetFile) = true
+TableTraits.isiterabletable(x::ParquetFile) = true
+
+function IteratorInterfaceExtensions.getiterator(file::ParquetFile)
+    p = ParFile(file.filename)
+
+    T_row_name = Symbol("RCType$(String(gensym())[3:end])")
+
+    schema(JuliaConverter(ParquetFiles), p, T_row_name)
+
+    T_row = eval(T_row_name)
+
+    col_names = fieldnames(T_row)
+    col_types = [i<:Vector{UInt8} ? String : i for i in T_row.types]
+
+    T = eval(:(@NT($(col_names...)))){col_types...}
+
+    rc = RecCursor(p, 1:nrows(p), colnames(p), JuliaBuilder(p, T_row))
+
+    it = ParquetNamedTupleIterator{T,T_row}(rc, nrows(p))
+    
+    return it
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,26 @@
 using ParquetFiles
+using NamedTuples
+import IterableTables
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@testset "ParquetFiles" begin
+
+parquet_pkg_dir = Pkg.dir("Parquet")
+
+include(joinpath(parquet_pkg_dir, "test", "get_parcompat.jl"))
+
+test_filename = joinpath(parquet_pkg_dir, "test", "parquet-compatibility", "parquet-testdata", "impala", "1.1.1-NONE", "nation.impala.parquet")
+
+pqf = ParquetFiles.ParquetFile(test_filename)
+
+@test IteratorInterfaceExtensions.isiterable(pqf) == true
+@test TableTraits.isiterabletable(pqf) == true
+
+it = IteratorInterfaceExtensions.getiterator(pqf)
+
+ar = collect(it)
+
+@test length(ar) == 25
+@test ar[1] == @NT(n_nationkey = 0, n_name = "ALGERIA", n_regionkey = 0, n_comment = " haggle. carefully final deposits detect slyly agai")
+
+end
