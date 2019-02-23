331709a1dccb480dd0b3469751f07e2340bbc0a8
David Anthoff <anthoff@berkeley.edu>
2019-02-23 12:48:43 -0800

    Add docs template

---
+++ docs/make.jl
@@ -0,0 +1,14 @@
+using Documenter, ParquetFiles
+
+makedocs(
+	modules = [ParquetFiles],
+	sitename = "ParquetFiles.jl",
+	analytics="UA-132838790-1",
+	pages = [
+        "Introduction" => "index.md"
+    ]
+)
+
+deploydocs(
+    repo = "github.com/queryverse/ParquetFiles.jl.git"
+)
