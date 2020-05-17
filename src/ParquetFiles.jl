614afb04f1ce12691f1575ea7efcb55292a946a2
tan <tanmaykm@gmail.com>
2020-05-17 10:43:27 +0530

    use Parquet.jl named tuples iterator: RecordCursor
    This [Parquet.jl update](https://github.com/JuliaIO/Parquet.jl/pull/71) will add a named tuple iterator `RecordReader`. We will be able to use that here directly, instead of wrapping over the older `RecCursor`.
    
    The new `map_logical_types` option to `ParFile` automatically converts byte arrays to strings, so we do not need to handle that here now.

--- src/ParquetFiles.jl
+++ src/ParquetFiles.jl
@@ -23,33 +23,6 @@ function Base.show(io::IO, ::MIME"application/vnd.dataresource+json", source::Pa
 end
 Base.Multimedia.showable(::MIME"application/vnd.dataresource+json", source::ParquetFile) = true
 
-struct ParquetNamedTupleIterator{T,T_row}
-    rc::RecCursor
-    nrows::Int
-end
-
-function Base.eltype(itr::ParquetNamedTupleIterator{T,T_row}) where {T,T_row}
-    return T
-end
-
-function Base.length(itr::ParquetNamedTupleIterator)
-    return itr.nrows
-end
-
-@generated function Base.iterate(itr::ParquetNamedTupleIterator{T,T_row}, state...) where {T,T_row}
-    names = fieldnames(T)
-    quote
-        y = iterate(itr.rc, state...)
-        if y === nothing
-            return nothing
-        else
-            v = y[1]
-            next_state = y[2]
-            return T(($([fieldtype(T, i) <: String ? :(String(copy(v.$(names[i])))) : :(v.$(names[i])) for i = 1:length(names)]...),)), next_state
-        end
-    end
-end
-
 function fileio_load(f::FileIO.File{FileIO.format"Parquet"})
     return ParquetFile(f.filename)
 end
@@ -58,24 +31,8 @@ IteratorInterfaceExtensions.isiterable(x::ParquetFile) = true
 TableTraits.isiterabletable(x::ParquetFile) = true
 
 function IteratorInterfaceExtensions.getiterator(file::ParquetFile)
-    p = ParFile(file.filename)
-
-    T_row_name = Symbol("RCType$(String(gensym())[3:end])")
-
-    schema(JuliaConverter(ParquetFiles), p, T_row_name)
-
-    T_row = eval(T_row_name)
-
-    col_names = fieldnames(T_row)
-    col_types = [i <: Vector{UInt8} ? String : i for i in T_row.types]
-
-    T = NamedTuple{(col_names...,),Tuple{col_types...}}
-
-    rc = RecCursor(p, 1:nrows(p), colnames(p), JuliaBuilder(p, T_row))
-
-    it = ParquetNamedTupleIterator{T,T_row}(rc, nrows(p))
-
-    return it
+    p = ParFile(file.filename; map_logical_types=true)
+    return RecordCursor(p)
 end
 
 end # module
